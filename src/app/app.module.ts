18e349626a14162e015dcb944db2c45383ffd5b2
harimasora <zidalo@gmail.com>
2019-03-19 11:43:45 -0300

    Initial setup for Firebase Native

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,14 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { Firebase } from '@ionic-native/firebase/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
+    Firebase,
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
