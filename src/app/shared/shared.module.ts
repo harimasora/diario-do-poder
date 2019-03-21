b01b15b1a47ca73a65fa0d4139dc283b621a38ad
harimasora <zidalo@gmail.com>
2019-03-21 18:11:03 -0300

    Header component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-	declarations: [ProfileComponent, LoginComponent],
+	declarations: [ProfileComponent, LoginComponent, HeaderComponent],
 	imports: [CommonModule, IonicModule],
-	exports: [ProfileComponent, LoginComponent],
+	exports: [ProfileComponent, LoginComponent, HeaderComponent],
 })
 export class SharedModule {}
