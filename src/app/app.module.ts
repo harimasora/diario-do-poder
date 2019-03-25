e240f6e741afcb01e64d675fca153b6acd40e711
harimasora <zidalo@gmail.com>
2019-03-25 12:59:17 -0300

    Wordpress service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -26,6 +27,7 @@ import { IonicStorageModule } from '@ionic/storage';
   entryComponents: [],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(),
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
---
+++ src/app/services/wordpress.service.ts
@@ -0,0 +1,109 @@
+import { Injectable } from '@angular/core';
+import {
+  BehaviorSubject,
+  of,
+  from,
+  Observable,
+  defer,
+  combineLatest,
+} from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { switchMap, mergeMap, map, tap } from 'rxjs/operators';
+
+const BASE_URL = 'https://diariodopoder.com.br/wp-json/wp/v2';
+const POSTS_URL = `${BASE_URL}/posts`;
+const CATEGORIES_URL = `${BASE_URL}/categories`;
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WordpressService {
+  posts$: BehaviorSubject<any[]> = new BehaviorSubject([]);
+
+  constructor(private http: HttpClient) {
+    this.getPosts()
+      .pipe(this.joinCategories())
+      .subscribe(posts => {
+        this.posts$.next(posts);
+      });
+  }
+
+  getPosts() {
+    return this.http.get(POSTS_URL);
+  }
+
+  getPost(id: string) {
+    return this.http.get(`${POSTS_URL}/${id}`);
+  }
+
+  getCategories(ids: number[]): Observable<any> {
+    if (ids) {
+      return from(ids).pipe(
+        mergeMap(id => this.http.get(`${CATEGORIES_URL}/${id}`)),
+      );
+    } else {
+      return this.http.get(CATEGORIES_URL);
+    }
+  }
+
+  getCategory(id: string) {
+    return this.http.get(`${CATEGORIES_URL}/${id}`);
+  }
+
+  private joinCategories = () => {
+    return (source: Observable<any>) => {
+      return defer<any>(() => {
+        // Operator state
+        let collectionData;
+        const cache = new Map();
+
+        const docField = 'categories';
+
+        return source.pipe(
+          switchMap(data => {
+            // Clear mapping on each emitted value
+            cache.clear();
+
+            // Save the parent data state
+            collectionData = data as any[];
+
+            const reads$ = [];
+            let i = 0;
+            for (const doc of collectionData) {
+              console.log('doc[docField]', doc[docField]);
+              // Categories from post
+              for (const id of doc[docField]) {
+                // Skip if doc field does not exist or is already in cache
+                if (cache.get(id)) {
+                  continue;
+                }
+
+                // Push doc read to Array
+                reads$.push(this.http.get(`${CATEGORIES_URL}/${id}`));
+                cache.set(id, i);
+              }
+
+              i++;
+            }
+
+            return reads$.length ? combineLatest(reads$) : of([]);
+          }),
+          map(joins => {
+            console.log('joins', joins);
+            return collectionData.map((v, i) => {
+              const categories = v[docField].map(id =>
+                joins.find(category => category.id === id),
+              );
+              return { ...v, categories: categories || null };
+            });
+          }),
+          tap(final => {
+            console.log(
+              `Queried ${(final as any).length}, Joined ${cache.size} docs`,
+            );
+          }),
+        );
+      });
+    };
+  };
+}
